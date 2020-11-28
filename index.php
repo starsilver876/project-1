0906633fe839c4560a0aa57564f9c5add0d7ec0d
starsilver876 <71236324+starsilver876@users.noreply.github.com>
2020-11-28 12:42:09 +0700

    belajar kapsulation

--- index.php
+++ index.php
@@ -21,4 +21,15 @@ $redmi->display = "6,5 inc";
 $redmi->ram = " 6 GB";
 $redmi->harga = "Rp.3.3500.000";
 
-echo "Tipe ".$redmi->KeceppatanKinerja();
+$redmi2 = new product;
+$redmi2->merk = "Redmi";
+$redmi2->tipe = "Remi 8 Pro";
+$redmi2->display = "6,5 inc";
+$redmi2->ram = " 6 GB";
+$redmi2->harga = "Rp.3.3500.000";
+
+echo "$redmi->tipe";
+echo "<br>";
+echo "$redmi->tipe";
+
+if
